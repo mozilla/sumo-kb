1011d5286af6b42d82b4a1e6084c18f9974b9ce0
JBMagination <jbmagination@jbmagination.com>
2022-10-03 00:57:35 -0400

    Create install-flatpak.sh

---
+++ run-firefox-chromeos/install-flatpak.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export $(cat /etc/os-release | grep VERSION_CODENAME);
+if [ $VERSION_CODENAME = bullseye ]
+then
+  echo 'deb http://deb.debian.org/debian bullseye-backports main' > /etc/apt/sources.list.d/backports.list
+  sudo apt update
+  sudo apt install flatpak/bullseye-backports -y
+else
+if [ $VERSION_CODENAME = buster ]
+then
+  echo 'deb http://deb.debian.org/debian buster-backports main' > /etc/apt/sources.list.d/backports.list
+  sudo apt update
+  sudo apt install flatpak/buster-backports -y
+else
+  echo 'This script does not support codename "'$VERSION_CODENAME'".'
