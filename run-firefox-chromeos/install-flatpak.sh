827e95d44792303965c80f7a0aa6270fcfac0a6e
JBMagination <jbmagination@jbmagination.com>
2022-10-03 01:11:30 -0400

    Update install-flatpak.sh

--- run-firefox-chromeos/install-flatpak.sh
+++ run-firefox-chromeos/install-flatpak.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export $(cat /etc/os-release | grep VERSION_CODENAME);
-if [ $VERSION_CODENAME = bullseye ]
+if [ "$VERSION_CODENAME" == bullseye ]
 then
   echo 'deb http://deb.debian.org/debian bullseye-backports main' > /etc/apt/sources.list.d/backports.list
   sudo apt update
   sudo apt install flatpak/bullseye-backports -y
 else
-if [ $VERSION_CODENAME = buster ]
+if [ "$VERSION_CODENAME" == buster ]
 then
   echo 'deb http://deb.debian.org/debian buster-backports main' > /etc/apt/sources.list.d/backports.list
   sudo apt update
